719cf508b99deb32c558f322d0dddda75cad0a2f
brunalimap <62139724+brunalimap@users.noreply.github.com>
2021-01-19 16:13:01 -0300

    Add files

---
+++ README.md
@@ -0,0 +1,30 @@
+# 1.0 Sobre o dataset
+
+Este conjunto de dados contém preços de venda de casas para King County, que inclui Seattle. Inclui casas vendidas entre maio de 2014 e maio de 2015.
+
+
+# 2.0 Biblioteca dos Dados
+
+- <b>id</b>: ID único para cada casa vendida
+- <b>date</b>: Data da venda da casa
+- <b>price</b>: Preço de cada casa vendida
+- <b>bedrooms</b>: Número de quartos
+- <b>bathrooms</b>: Número de banheiros, onde 0,5 corresponde a um quarto com banheiro, mas sem chuveiro
+- <b>sqft_living</b>:  Metragem quadrada do espaço interior dos apartamentos
+- <b>sqft_lot</b>: Metragem quadrada do espaço terrestre
+- <b>floors</b>: Número de andares
+- <b>waterfront</b>: -  Uma variável fictícia para saber se o apartamento tinha vista para a beira-mar ou não
+- <b>view</b>: Um índice de 0 a 4 de quão boa era a vista da propriedade
+- <b>condition</b>: - Um índice de 1 a 5 sobre o estado do apartamento,
+- <b>grade</b>: Um índice de 1 a 13, onde 1-3 fica aquém da construção e design do edifício, 7 tem um nível médio de 1. nível de construção e design e 11-13 tem um nível de construção e design de alta qualidade.
+- <b>sqft_above</b>: A metragem quadrada do espaço habitacional interno que está acima do nível do solo
+- <b>sqft_basement</b>: A metragem quadrada do espaço habitacional interno que está abaixo do nível do solo
+- <b>yr_built</b>: O ano em que a casa foi construída inicialmente
+- <b>yr_renovated</b>: O ano da última renovação da casa
+- <b>zipcode</b>: Em qual CEP se encontra a casa
+- <b>lat</b>: Latitude
+- <b>long</b>: Longitude
+- <b>sqft_living15</b>: A metragem quadrada do espaço interno de habitação para os 15 vizinhos mais próximos 
+- <b>sqft_lot15</b>: A metragem quadrada dos lotes de terreno dos 15 vizinhos mais próximos.
+
+
---
+++ aula_python01.py
@@ -0,0 +1,83 @@
+import pandas as pd
+
+
+df = pd.read_csv('data/kc_house_data.csv')
+
+# Para verificar o dataset
+#print(df.head())
+
+# Para verificar os tipos das variáveis das colunas
+#print(df.dtypes)
+
+# Primeiras Perguntas dos CEO
+
+# 1- Quantas casas estão disponíveis para comprar?
+print("\nQuantidade de casas disponíveis para compra: {}".format(df.shape[0]))
+print("\n")
+
+# 2- Quantos atributos as casas possuem?
+print("\nQuantidade de atributos as casas possuem: {}".format(df.shape[1]))
+print("\n")
+
+# 3- Quais são os atributos das casas?
+print("\nOs atributos são: {}".format(df.columns))
+print("\n")
+
+# 4- Qual a casa mais cara ( casa com o maior valor de venda)?
+#preciso que mostre o ID da casa
+sales = df[['id','price']].sort_values('price', ascending=False)
+print("\n A casa com o maior valor de venda é \n {}".format(sales.head(1)))
+print("\n")
+
+
+# 5- Qual a casa com o  maior número de quartos?
+bedroom = df[['id','bedrooms']].sort_values('bedrooms',ascending=False)
+print("\n A casa com o maior quantidade de quartos: \n {}".format(bedroom.head(1)))
+print("\n")
+
+# 6- Qual a soma total de quartos do conjunto de dados?
+print('Total de quartos que tem no dataset: {}'.format(df['bedrooms'].sum()))
+print("\n")
+
+# 7- Quantas casas possuem 2 banheiros?
+a = df.query('bathrooms == 2')['bathrooms'].count()
+print('Quantidade de casas com 2 banheiros no dataset: {}'.format(a))
+print("\n")
+
+# 8- Qual o preço médio de todas as casas no conjunto de dados?
+b = df['price'].mean()
+print('Preço médio de todas as casas: {}'.format(round(b,2)))
+print("\n")
+
+# 9- Qual o preço médio de casas com 2 banheiros?
+c = df.loc[df['bathrooms'] == 2, 'price'].mean()
+print('Preço médio das casas com 2 banheiro: {}'.format(round(c,2)))
+print("\n")
+
+
+# 10- Qual o preço mínimo entre as casas com 3 quartos?
+d = df.query('bedrooms == 3')['price'].min()
+print('Preço minimo em casas com dois andares: {}'.format(round(d,2)))
+print("\n")
+
+# 11- Quantas casas possuem mais de 300 metros quadrados na sala de estar?
+df['m2_living'] = df['sqft_living']*0.092
+print('Quantidade de casa que possuem mais de 300 metros quadrados é {}'.format(df[df['m2_living']>300].shape[0]))
+print("\n")
+
+# 12- Quantas casas tem mais de 2 andares?
+e = df[df['floors'] > 2].shape[0]
+print('Quantidade de casas com mais de 2 andares no dataset: {}'.format(e))
+print("\n")
+
+# 13- Quantas casas tem vista para o mar?
+print('Casas com vista para o mar: {}'.format(df[df['waterfront']==1].shape[0]))
+print("\n")
+
+# 14- Das casas com vista para o mar, quantas tem 3 quartos?
+f = df[(df['waterfront'] == 1) & (df['bedrooms'] == 3)].shape[0]
+print('Casas com vista para o mar e com 3 quartos: {}'.format(f))
+print("\n")
+
+# 15- Das casas com mais de 300 metros quadrados de sala de estar, quantas tem mais de 2 banheiros banheiros?
+print('Total de com mais de 2 banheiros: {}'.format(df[(df['m2_living'] > 300) & (df['bathrooms']> 2)].shape[0]))
